28902cc6751ff1db8bb78ba0561d789279a2b0e0
{manal{} <{f200141@cfd.nu.edu.pk}>
2023-11-12 01:21:20 +0500

    Register user module / added db config file

--- project/src/DataAccessLayer/DBhandler.java
+++ project/src/DataAccessLayer/DBhandler.java
@@ -1,16 +1,21 @@
 package DataAccessLayer;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 // Data Access and Domain layer
 public class DBhandler implements UserDataAccess {
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/FYP";
-    private static final String DATABASE_USER = "root";
-    private static final String DATABASE_PASSWORD = "";
+    private static final String DATABASE_PROPERTIES = "config.properties";
+
+    private String DATABASE_URL;
+    private String DATABASE_USER;
+    private String DATABASE_PASSWORD;
 
     static {
         try {
@@ -23,6 +28,7 @@ public class DBhandler implements UserDataAccess {
     }
 
     public DBhandler() {
+        loadDatabaseProperties();
         // Ensure the static block is executed
         System.out.println("JDBC driver loaded successfully.");
 
@@ -33,7 +39,7 @@ public class DBhandler implements UserDataAccess {
                     + "email VARCHAR(255) NOT NULL,"
                     + "password VARCHAR(255) NOT NULL,"
                     + "phoneNumber VARCHAR(20) NOT NULL)";
-            
+
             try (Statement statement = connection.createStatement()) {
                 statement.executeUpdate(createUserTableQuery);
             }
@@ -42,11 +48,33 @@ public class DBhandler implements UserDataAccess {
         }
     }
 
+    private void loadDatabaseProperties() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(DATABASE_PROPERTIES)) {
+            Properties properties = new Properties();
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + DATABASE_PROPERTIES);
+                return;
+            }
+
+            // Load a properties file from class path, inside static method
+            properties.load(input);
+
+            // Get property values
+            DATABASE_URL = properties.getProperty("database.url");
+            DATABASE_USER = properties.getProperty("database.user");
+            DATABASE_PASSWORD = properties.getProperty("database.password");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     @Override
     public void saveUser(String name, String email, String password, String phoneNumber) {
         try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD)) {
             String insertUserQuery = "INSERT INTO users (name, email, password, phoneNumber) VALUES (?, ?, ?, ?)";
-            
+
             try (PreparedStatement preparedStatement = connection.prepareStatement(insertUserQuery)) {
                 preparedStatement.setString(1, name);
                 preparedStatement.setString(2, email);
@@ -65,10 +93,10 @@ public class DBhandler implements UserDataAccess {
 
         try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD)) {
             String getAllUsersQuery = "SELECT * FROM users";
-            
+
             try (Statement statement = connection.createStatement()) {
                 ResultSet resultSet = statement.executeQuery(getAllUsersQuery);
-                
+
                 while (resultSet.next()) {
                     Map<String, String> user = new HashMap<>();
                     user.put("name", resultSet.getString("name"));
